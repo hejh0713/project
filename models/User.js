4c5115b086ad3ee35bfe39d7b943cebf809d6718
unknown <wjdwlgh07139@gmail.com>
2021-11-06 16:16:08 +0900

    로그인 만듬

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
     name: {
@@ -16,7 +19,7 @@ const userSchema = mongoose.Schema({
     },
     lastname: {
         type: String,
-        maxlength: 50
+        maxlength: 70
     },
     role: {
         type: Number,
@@ -30,6 +33,42 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+
+
+userSchema.pre('save', function(next){
+    var user = this;
+    if(user.isModified('password')){
+        //비밀번호를 암호화시킨다
+        bcrypt.genSalt(saltRounds, function (err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }else{
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //plainpassword 1234567 암호화된 비밀번호 : $2b$10$a/lTfJ0Vy0.nK2AJaZLXUuB2EFVU5DDGWy0Q.lu8yDR2mTU2tRGCe
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user =this;
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+    user.token = token
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    })
+}
 
 const User = mongoose.model('User', userSchema)
 
